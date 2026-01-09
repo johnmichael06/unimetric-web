a8ba82a8608d2e300525f72fe270f34aec4015bd
jmfrivera <150635986+jmfrivera@users.noreply.github.com>
2026-01-09 18:55:50 +0800

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// 1. IMPORT THIS
+import { Analytics } from "@vercel/analytics/react";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+    {/* 2. ADD COMPONENT HERE */}
+    <Analytics />
+  </React.StrictMode>
+);
